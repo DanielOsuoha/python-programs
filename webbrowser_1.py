9e2a6e44e2caa6d10ee58d8d72e4acda187fea14
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2024-05-04 08:28:19 +0000

    Completed the web browser

--- webbrowser_1.py
+++ webbrowser_1.py
@@ -1,27 +1,51 @@
 from PyQt5.QtWidgets import *
-# from PyQt5.QtGui import *
+from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
+
 class MyWebBrowser(QMainWindow):
     def __init__(self): 
         super(MyWebBrowser, self).__init__()       
+        
         self.window = QWidget()
         self.window.setWindowTitle("My Web Browser")
         self.layout = QVBoxLayout()
         self.horizontal = QHBoxLayout()
         
         self.url_bar = QTextEdit()
-        self.url_bar.setMaximumHeight(28)
-        self.forward_btn = QPushButton("Forward")
+        self.url_bar.setMaximumHeight(28)  
+        # Apply style to the QTextEdit
+        self.url_bar.setStyleSheet(
+            "QTextEdit { border: 1px solid #ccc; border-radius: 8px; padding: 4px; font-size: 14px; }"
+        )
+        
+        
+        self.forward_btn = QPushButton(" --> ")
         self.forward_btn.setMinimumHeight(28)
-        
-        
-        self.back_btn = QPushButton("Back")
+        self.forward_btn.setStyleSheet(
+            "QPushButton { border-radius: 5px; background-color: rgb(200,223,223); }"
+            "QPushButton:hover { background-color: #bbb; }"
+        )
+      
+        self.back_btn = QPushButton(" <-- ")
         self.back_btn.setMinimumHeight(28)
+        self.back_btn.setStyleSheet(
+            "QPushButton { border-radius: 5px; background-color: rgb(200,223,223); }"
+            "QPushButton:hover { background-color: #bbb; }"
+        )
         
-        
-        self.go_btn = QPushButton("Go")
+        search_icon_path = "images.png"
+
+        # Create a QPushButton with a search icon
+        self.go_btn = QPushButton()
+        self.go_btn.setIcon(QIcon(search_icon_path))
         self.go_btn.setMinimumHeight(28)
+        self.go_btn.setStyleSheet(
+            "QPushButton { border-radius: 5px; background-color: rgb(250,250,250); }"
+            "QPushButton:hover { background-color: rgb(170,134,200); }"
+        )
+        # Connect returnPressed signal of QTextEdit to slot for go_btn click
+        # self.url_bar.returnPressed.connect(self.go_btn.click)
         
         self.horizontal.addWidget(self.back_btn)
         self.horizontal.addWidget(self.forward_btn)
@@ -55,3 +79,6 @@ class MyWebBrowser(QMainWindow):
 app = QApplication([])
 window = MyWebBrowser()
 app.exec_()
+
+
+# Created by Daniel Osuoha
