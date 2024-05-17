149ed6dfa5bd472d073714cb939e9a3018765253
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2024-05-17 05:04:19 +0000

    better code

--- json_file.py
+++ json_file.py
@@ -2,7 +2,7 @@ import json
 
 data = '''
     {
-        "name" : "Chuck",
+        "name" : "Daniel",
         "phone" : {
             "type" : "intl",
             "number" : "+1 734 303 4456"
@@ -13,8 +13,31 @@ data = '''
     }
 '''
 
+
 info = json.loads(data)
 
 print("Name: ", info['name'])
 print("Phone Number:", info['phone']['number'])
 print("Hidden:", info['email']['hide'])
+
+
+input = '''
+    [
+        {
+            "id" : "001",
+            "x" : "2",
+            "name" : "Chuck"
+        },
+        {
+            "id" : "009",
+            "x" : "7",
+            "name" : "Brent"
+        }
+    ]
+'''
+
+info = json.loads(input)
+
+print("Users:", len(info))
+for i in info:
+    print(i['id'], i['x'], i['name'])
--- web_browser.py
+++ web_browser.py
@@ -1,11 +1,36 @@
 import socket
+
+# Create a socket object
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-mysock.send(b'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\n\n')
-#this is the same as cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0'.encode(); mysock.send(cmd) 
+
+# Connect to the server
+mysock.connect(('picoctf.org', 443))
+
+# Send the HTTP GET request
+request = "GET /resources.html HTTP/1.1\r\nHost: picoctf.org\r\n\r\n"
+mysock.send(request.encode())
+
+# Receive and print the response
 while True:
     data = mysock.recv(512)
-    if (len(data) < 1):
+    if not data:
         break
-    print(data.decode())
+    print(data.decode(), end='')
+
+# Close the socket
 mysock.close()
+
+
+
+
+# import socket
+# mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# mysock.connect(('picoctf.org', 443))
+# mysock.send(b'GET https://picoctf.org/resources.html HTTP/1.0\n\n')
+# #this is the same as cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0'.encode(); mysock.send(cmd) 
+# while True:
+#     data = mysock.recv(512)
+#     if (len(data) < 1):
+#         break
+#     print(data.decode())
+# mysock.close()
