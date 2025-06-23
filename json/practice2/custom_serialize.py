7cc6a1d6821802e3dede04132ceb3892cce78f1a
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-06-22 21:53:37 -0700

    made changes to main.py

--- json/practice2/custom_serialize.py
+++ json/practice2/custom_serialize.py
@@ -1,11 +1,12 @@
 import json
 from event import Event
 from datetime import datetime
-def custom_serialize(name, date_time):
-    pass
+def custom_serialize(object):
+    date, time = str(object.time).split()
+    return {"name": object.name, "time":time}
 
 if __name__ == "__main__":
-    event = Event("Hackathon", datetime.now)
-    print(event.time)
-    # value = json.dumps(event, default=custom_serialize)
-    # print(value)
+    event = Event("Hackathon", datetime.now())
+    # print(event.time)
+    value = json.dumps(event, default=custom_serialize)
+    print(value)
