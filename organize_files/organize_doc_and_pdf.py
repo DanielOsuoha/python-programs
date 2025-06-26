da684294cc1454ebc001d92722b6a9f430873775
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-06-26 12:28:00 -0700

    Update organize_doc_and_pdf.py

--- organize_files/organize_doc_and_pdf.py
+++ organize_files/organize_doc_and_pdf.py
@@ -3,16 +3,13 @@ import os, shutil
 path = input("Enter the path to the directory you want to operate on: ")
 if os.path.exists(path):
     if os.path.isdir(path):
-        #create the directories if they do not exist
         if not os.path.exists(os.path.join(path, 'pdf')):
             os.mkdir(os.path.join(path, 'pdf'))
         if not os.path.exists(os.path.join(path, 'doc')):
             os.mkdir(os.path.join(path, 'doc'))
         
         files = os.listdir(path)
-        # print(files)
         for file in files:
-            #print(file, file.endswith('.pdf'))
             if file.endswith('.pdf'):
                 #add to the pdf directory
                 shutil.move(os.path.join(path, file), os.path.join(path, 'pdf'))
