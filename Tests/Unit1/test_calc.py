de12d55e136ea5945158e25ac923473a27a128eb
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-09-20 10:42:55 -0500

    created test for subtract

--- Tests/Unit1/test_calc.py
+++ Tests/Unit1/test_calc.py
@@ -5,3 +5,10 @@ class TestCalc(unittest.TestCase):
     def test_add(self):
         result = calc.add(10, 5)
         self.assertEqual(result, 15)
+
+    def test_subtract(self):
+        result = calc.substract(4, 2)
+        self.assertEqual(result, 2)
+
+if __name__ == "__main__":
+    unittest.main()
