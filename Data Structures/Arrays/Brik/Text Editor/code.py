8cc9a7004edef9726748ade4688d1aff07686eb2
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-10-12 04:47:40 -0500

    slight changes to test_calc

--- Data Structures/Arrays/Brik/Text Editor/code.py
+++ Data Structures/Arrays/Brik/Text Editor/code.py
@@ -1,9 +1,12 @@
 class TextEditor:
+    from sortedcontainers import SortedList
     def __init__(self):
-        self.editor = []
+        self.editor = SortedList()
     
     def insert(self, offset, text):
-        pass
+        index = len(self.editor)
+        for i in range(len(text)):
+            self.editor.add((index+i, text[i]))
 
     def delete(self, offset, length):
         pass
