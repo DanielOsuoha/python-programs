78ea99ac07101da2647e07e80fe74e392eee6332
Daniel Osuoha <dan646655@gmail.com>
2025-10-15 04:24:57 -0500

    make changes

---
+++ Data Structures/Dots&Boxes/code.py
@@ -0,0 +1,13 @@
+"""
+You are to implement the core logic for the game Dots and Boxes.
+
+There are two players who take turns connecting two adjacent dots on an n × n grid. A move is represented by the coordinates of the two dots being connected. If a player completes a box (meaning they place the fourth and final edge around a square), they immediately win the game.
+
+Your function should process a sequence of moves and determine if any player completes a box. If a box is formed, return the name of the player who won. Otherwise, continue until all moves are processed.
+
+You can assume input will either come interactively (via input()) or from an array of moves.
+"""
+
+
+def main():
+    pass
